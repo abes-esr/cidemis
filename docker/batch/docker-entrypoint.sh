0e12c3808d9f9a2f2709e5849ee77027f8943a62
pierre-maraval <pierre.maraval@gmail.com>
2024-02-15 16:01:26 +0100

    Modif entrypoint pour ne pas exécuter les batch si variable d'env à 0

--- docker/batch/docker-entrypoint.sh
+++ docker/batch/docker-entrypoint.sh
@@ -3,16 +3,20 @@
 # Paramètres par défaut du conteneur
 export CIDEMIS_BATCH_CRON_STATS=${CIDEMIS_BATCH_CRON_STATS:='50 8 1 * *'}
 export CIDEMIS_BATCH_CRON_MAILING=${CIDEMIS_BATCH_CRON_MAILING:='50 8 1 * *'}
+export CIDEMIS_BATCH_LAUNCH=${CIDEMIS_BATCH_LAUNCH:=0}
 
 # Réglage de /etc/environment pour que les crontab s'exécutent avec les bonnes variables d'env
 echo "$(env)
 LANG=fr_FR.UTF-8" > /etc/environment
 
-# Charge la crontab depuis le template
-envsubst < /etc/cron.d/tasks.tmpl > /etc/cron.d/tasks
-echo "-> Installation des crontab :"
-cat /etc/cron.d/tasks
-crontab /etc/cron.d/tasks
+if [ CIDEMIS_BATCH_LAUNCH -eq 1 ];
+then
+  # Charge la crontab depuis le template
+  envsubst < /etc/cron.d/tasks.tmpl > /etc/cron.d/tasks
+  echo "-> Installation des crontab :"
+  cat /etc/cron.d/tasks
+  crontab /etc/cron.d/tasks
 
-# execute CMD (crond)
-exec "$@"
+  # execute CMD (crond)
+  exec "$@"
+fi
