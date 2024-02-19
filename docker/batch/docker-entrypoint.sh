374873889011dc0b80e9508d5372d6db95753e8b
pierre-maraval <pierre.maraval@gmail.com>
2024-02-19 08:14:28 +0100

    Fix docker-entrypoint.sh

--- docker/batch/docker-entrypoint.sh
+++ docker/batch/docker-entrypoint.sh
@@ -3,13 +3,13 @@
 # Paramètres par défaut du conteneur
 export CIDEMIS_BATCH_CRON_STATS=${CIDEMIS_BATCH_CRON_STATS:='50 8 1 * *'}
 export CIDEMIS_BATCH_CRON_MAILING=${CIDEMIS_BATCH_CRON_MAILING:='50 8 1 * *'}
-export CIDEMIS_BATCH_LAUNCH=${CIDEMIS_BATCH_LAUNCH:=0}
+export CIDEMIS_BATCH_LAUNCH=${CIDEMIS_BATCH_LAUNCH:='0'}
 
 # Réglage de /etc/environment pour que les crontab s'exécutent avec les bonnes variables d'env
 echo "$(env)
 LANG=fr_FR.UTF-8" > /etc/environment
 
-if [ $CIDEMIS_BATCH_LAUNCH -eq 1 ];
+if [ "$CIDEMIS_BATCH_LAUNCH" = "1" ];
 then
   # Charge la crontab depuis le template
   envsubst < /etc/cron.d/tasks.tmpl > /etc/cron.d/tasks
