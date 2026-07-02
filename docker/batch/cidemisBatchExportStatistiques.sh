b1124fec24af5c34f923bcc1f95baf4bbcdd182a
pierre-maraval <pierre.maraval@gmail.com>
2026-07-02 11:27:45 +0200

    Fix incorrect `--spring.batch.job.names` argument in batch scripts (`mailing` and `exportStatistiques`) to `--spring.batch.job.name`

--- docker/batch/cidemisBatchExportStatistiques.sh
+++ docker/batch/cidemisBatchExportStatistiques.sh
@@ -1,5 +1,5 @@
 LANG=fr_FR.UTF-8
 if [[ $(pgrep -cf "cidemis-batch.jar --spring.batch.job.names=exportStatistiques") = 0 ]];
 then
-    java -Djava.security.egd=file:///dev/urandom -jar cidemis-batch.jar --spring.batch.job.names=exportStatistiques
+    java -Djava.security.egd=file:///dev/urandom -jar cidemis-batch.jar --spring.batch.job.name=exportStatistiques
 fi
--- docker/batch/cidemisBatchMailCIEPS.sh
+++ docker/batch/cidemisBatchMailCIEPS.sh
@@ -1,4 +1,4 @@
 if [[ $(pgrep -cf "cidemis-batch.jar --spring.batch.job.names=mailing") = 0 ]];
 then
-    java -Djava.security.egd=file:///dev/urandom -jar cidemis-batch.jar --spring.batch.job.names=mailing
+    java -Djava.security.egd=file:///dev/urandom -jar cidemis-batch.jar --spring.batch.job.name=mailing
 fi
